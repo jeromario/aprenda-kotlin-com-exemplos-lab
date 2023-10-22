a65286b00610b133119b25364bb36c66f7a2c1b7
Jefferson Andrade <jeffersonjla@hotmail.com>
2023-10-22 12:01:52 -0300

    Implementando o desafio

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,46 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-class Usuario
+data class Usuario(val nome: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, var duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, val nivel: Nivel, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    fun matricular(vararg usuarios: Usuario) {
+        for ( usuario in usuarios){
+            inscritos.add(usuario)
+            println("${usuario.nome}, matriculado na formação de ${this.nome} com sucesso!")
+            
+        }
+        
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+   
+    val java = ConteudoEducacional("Java",120)
+    val python = ConteudoEducacional("Python", 120)
+    val mysql = ConteudoEducacional("MySQL")
+      
+    val backend = Formacao("Back-End",Nivel.AVANCADO,mutableListOf(java,mysql))
+    val dataScientist = Formacao("Data Scientist", Nivel.INTERMEDIARIO,mutableListOf(python,mysql))
+    
+    val aluno1 = Usuario("Jefferson")
+    val aluno2 = Usuario("Sandrele")
+    val aluno3 = Usuario("Beatriz")
+    
+    println(backend)
+    println(dataScientist)
+    println()
+    backend.matricular(aluno1,aluno2)
+    println()
+    println("Alunos inscritos no curso de Back-End: ${backend.inscritos}")
+    println()
+    dataScientist.matricular(aluno3,aluno1)
+    println()
+    println("Alunos inscritos no curso de Data Scientist: ${dataScientist.inscritos}")
 }
